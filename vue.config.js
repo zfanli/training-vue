1471867a6ee1a83ae9fbe66bae1208dd69e23b60
Richard <cyanyao@live.com>
2018-11-29 18:09:39 +0800

    fixes runtime-only build

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 // config for vue.js
-
 module.exports = {
   devServer: {
     port: 4000,
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        vue$: 'vue/dist/vue.esm.js'
+      }
+    }
   }
 }
