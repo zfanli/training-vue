30e858f3101218749232d10289440e085f59f9cb
Richard <cyanyao@live.com>
2018-11-30 18:18:20 +0800

    add new view for display articles

---
+++ src/views/PostViewer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{title}}</div>
+</template>
+<script>
+export default {
+  computed: {
+    title: function() {
+      return this.$route.params.title
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
