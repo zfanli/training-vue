c834785ab245a983255ad201aae32cf61f0ca2ab
Richard <cyanyao@live.com>
2018-12-01 15:23:15 +0800

    404 handler

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="page-not-found">
+    <div>Page Not Found.</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.page-not-found {
+  flex-grow: 1;
+  flex-shrink: 0;
+}
+</style>
