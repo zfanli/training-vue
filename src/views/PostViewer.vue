8585d48199df581920191b5a9d716f0f50c9226a
Richard <cyanyao@live.com>
2018-12-01 08:56:31 +0800

    receice props to instead of use $route.params and get post from list

--- src/views/PostViewer.vue
+++ src/views/PostViewer.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>{{title}}</div>
+  <div>{{post.lastUpdatedTime}}</div>
 </template>
 <script>
 export default {
+  props: {
+    id: String,
+  },
   computed: {
-    title: function() {
-      return this.$route.params.title
-    }
-  }
+    post: function() {
+      const id = this.id
+      const post = this.$store.state.list.filter(function(p) {
+        return String(p.lastUpdatedTime) === id
+      })
+      if (post.length !== 1) {
+        this.$router.push('/404')
+        return
+      }
+      return post[0]
+    },
+  },
 }
 </script>
 <style lang="scss" scoped>
