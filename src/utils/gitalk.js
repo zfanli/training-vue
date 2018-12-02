9e21370f80bab5df1b48bfd179a44a1611f82a7f
Richard <cyanyao@live.com>
2018-12-02 21:48:30 +0800

    add gitalk tool

---
+++ src/utils/gitalk.js
@@ -0,0 +1,12 @@
+import 'gitalk/dist/gitalk.css'
+import Gitalk from 'gitalk'
+
+export default new Gitalk({
+  clientID: 'de47adf4f9d0394257eb',
+  clientSecret: '7619f44e8a16d8d6b450a588caf2e087f1fb8500',
+  repo: 'comments',
+  owner: 'zfanli',
+  admin: ['zfanli'],
+  id: location.pathname, // Ensure uniqueness and length less than 50
+  distractionFreeMode: false, // Facebook-like distraction free mode
+})
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 import markdown from './markdown'
 import highlight from './highlight'
+import gitalk from './gitalk'
 
 // reorganize module
-export { markdown, highlight }
+export { markdown, highlight, gitalk }
