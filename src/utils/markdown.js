4da2cb82fbe1e0ab65c043f212b7d74e164ed21e
Richard <cyanyao@live.com>
2018-12-02 20:38:23 +0800

    add langPrefix option for support highlight tool to generate line numbers

--- src/utils/markdown.js
+++ src/utils/markdown.js
@@ -1,5 +1,11 @@
 import Md from 'markdown-it'
 
-const md = new Md()
+const md = new Md({
+  // Add `line-numbers` class for tell it to display the line numbers.
+  // This is just a flag, and do not mean line numbers must be displayed.
+  // The true trigger of line numbers' display is at `highlight.js`.
+  // Check if the line numbers relevant import was imported correctly.
+  langPrefix: 'line-numbers language-',
+})
 
 export default md.render.bind(md)
