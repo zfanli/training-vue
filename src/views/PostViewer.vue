1479b58fd8fd22ee257145165f1ca7316ce7ae30
Richard <cyanyao@live.com>
2018-12-02 10:56:53 +0800

    initial markdown support

--- src/views/PostViewer.vue
+++ src/views/PostViewer.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
     <div v-if="hasError">Something went wrong.</div>
-    <div v-else>{{post.lastUpdatedTime}}</div>
+    <div class="post" v-else>
+      <div class="post-title">{{ post.title }}</div>
+      <div class="post-body" v-html="renderMarkdown(post.body)"/>
+    </div>
   </div>
 </template>
+
 <script>
+import { markdown } from '../utils'
+
 export default {
   props: {
     id: String,
@@ -25,7 +31,11 @@ export default {
       return false
     },
   },
+  methods: {
+    renderMarkdown: markdown,
+  },
 }
 </script>
+
 <style lang="scss" scoped>
 </style>
