781395bb1f77ca7db778f1e2d9eaac3b1f48b787
Richard <cyanyao@live.com>
2018-12-03 12:31:26 +0800

    add task lists plugin to markdown parser and fix some typos

--- src/utils/markdown.js
+++ src/utils/markdown.js
@@ -1,4 +1,5 @@
 import Md from 'markdown-it'
+import taskLists from 'markdown-it-task-lists'
 
 const md = new Md({
   // Add `line-numbers` class for tell it to display the line numbers.
@@ -6,6 +7,6 @@ const md = new Md({
   // The true trigger of line numbers' display is at `highlight.js`.
   // Check if the line numbers relevant import was imported correctly.
   langPrefix: 'line-numbers language-',
-})
+}).use(taskLists)
 
 export default md.render.bind(md)
