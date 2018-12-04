e819a416df1bd6dfbca50fc6816007bb506f146b
Richard <cyanyao@live.com>
2018-12-05 00:14:23 +0800

    add pagination action and comments

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,14 @@
-// Action: fetch the article by specific name
+/**
+ * Action: fetch the article by given name and set it to post object by id.
+ */
 export const ACTION_FETCH_ARTICLE = 'ACTION_FETCH_ARTICLE'
-// Mutation: set payload.article to state.list[id].article
-export const SET_ARTICLE = 'SET_ARTICLE'
+
+/**
+ * Mutation: update article; set `payload.article` to `state.list[id].article`.
+ */
+export const MUTATION_SET_ARTICLE = 'MUTATION_SET_ARTICLE'
+
+/**
+ * Mutation: increase the `state.size` to show more posts.
+ */
+export const MUTATION_SHOW_MORE_POSTS = 'MUTATION_SHOW_MORE_POSTS'
