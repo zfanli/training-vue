42e809d06981fdd4e574c95a50179a40d0e1414e
Richard <cyanyao@live.com>
2018-12-04 17:39:57 +0800

    add action constants

---
+++ src/actions.js
@@ -0,0 +1,4 @@
+// Action: fetch the article by specific name
+export const ACTION_FETCH_ARTICLE = 'ACTION_FETCH_ARTICLE'
+// Mutation: set payload.article to state.list[id].article
+export const SET_ARTICLE = 'SET_ARTICLE'
