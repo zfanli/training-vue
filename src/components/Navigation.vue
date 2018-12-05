bda9fdd6380ac16df00adf2614a308f71f4a4688
Richard <cyanyao@live.com>
2018-12-05 18:04:56 +0800

    try to add a search bar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,6 +2,20 @@
   <nav class="nav">
     <div class="nav-body">
       <router-link class="logo" to="/">{{ title }}</router-link>
+      <div class="nav-search">
+        <transition-group name="fade" mode="out-in">
+          <div v-if="!search" @click="search = !search" key="label">SEARCH</div>
+          <input
+            class="search-box"
+            type="text"
+            v-else
+            v-model="input"
+            placeholder="Search"
+            @blur="search = !search"
+            key="input"
+          >
+        </transition-group>
+      </div>
     </div>
   </nav>
 </template>
@@ -12,6 +26,8 @@ export default {
   data: function() {
     return {
       title: this.$store.state.title,
+      input: '',
+      search: false,
     }
   },
 }
@@ -29,12 +45,38 @@ export default {
   .nav-body {
     font-family: 'Courier New', Courier, monospace;
     flex-grow: 1;
+    display: flex;
 
     .logo {
       color: white;
       cursor: pointer;
       text-decoration: none;
       text-transform: lowercase;
+    }
+
+    .nav-search {
+      margin-left: auto;
+
+      .search-box {
+        border: none;
+        background-color: #333;
+        transition: all 0.2s linear;
+        padding: 0.5rem;
+        outline: none;
+        border-radius: 5px;
+        color: white;
+        font-family: inherit;
+        text-align: center;
+
+        &::placeholder {
+          color: inherit;
+        }
+
+        &:focus {
+          background-color: white;
+          color: #333;
+        }
+      }
     }
   }
 }
