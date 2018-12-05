00608e29b32a411ba6a431b2c6ebb9cd3e344bb3
Richard <cyanyao@live.com>
2018-12-06 00:36:16 +0800

    update search panel

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,29 +3,41 @@
     <div class="nav-body">
       <router-link class="logo" to="/">{{ title }}</router-link>
       <div class="nav-search">
-        <transition-group name="fade" mode="out-in">
-          <div
-            class="search-label"
-            v-if="!search"
-            @click="search = !search"
-            key="search-label"
-          >search</div>
-          <input
-            class="search-box"
-            type="text"
-            v-else
-            v-model="input"
-            placeholder="Search"
-            @blur="search = !search"
-            key="search-input"
-          >
-        </transition-group>
+        <keep-alive>
+          <transition name="s" mode="out-in">
+            <div
+              class="search-label search-tansition"
+              v-if="!search"
+              @click="search = !search"
+              key="search-label"
+            >search</div>
+            <input
+              class="search-box search-tansition"
+              type="text"
+              v-else
+              v-focus
+              v-model="input"
+              placeholder="Search"
+              @blur="search = !search"
+              key="search-input"
+            >
+          </transition>
+        </keep-alive>
+        <ul class="search-result" v-if="searchResult">
+          <li
+            class="search-result-item"
+            v-for="post in searchResult"
+            :key="post.createdTimestamp"
+          >{{ post.title }}</li>
+        </ul>
       </div>
     </div>
   </nav>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'Navigation',
   data: function() {
@@ -34,6 +46,24 @@ export default {
       input: '',
       search: false,
     }
+  },
+  computed: {
+    ...mapState(['list']),
+    searchResult() {
+      const i = this.input
+      if (i && this.search) {
+        const posts = Object.values(this.list)
+        return posts.filter(p => new RegExp(i).test(p.title))
+      }
+      return false
+    },
+  },
+  directives: {
+    focus: {
+      inserted(el) {
+        el.focus()
+      },
+    },
   },
 }
 </script>
@@ -57,18 +87,24 @@ export default {
       cursor: pointer;
       text-decoration: none;
       text-transform: lowercase;
+      flex-shrink: 0;
     }
 
     .nav-search {
       margin-left: auto;
       font-family: inherit;
+      align-self: flex-end;
 
       .search-label {
         color: white;
+        width: 10rem;
+        transition: all 0.1s linear;
+        text-align: center;
       }
       .search-box {
+        width: 10rem;
         border: none;
-        transition: all 0.2s linear;
+        transition: all 0.1s linear;
         padding: 0.5rem;
         outline: none;
         border-radius: 5px;
@@ -80,7 +116,45 @@ export default {
           color: inherit;
         }
       }
+      .search-result {
+        position: absolute;
+        background-color: white;
+        display: block;
+        top: 45px;
+        right: 50px;
+        width: 15rem;
+        padding: 0 0.5rem;
+
+        .search-result-item {
+          list-style: none;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+          overflow: hidden;
+
+          &:not(:last-child) {
+            border-bottom: 1px solid #333;
+          }
+        }
+      }
     }
   }
+}
+</style>
+<style lang="scss">
+.search-tansition.s-enter {
+  transform: translate3d(10%, 0, 0);
+  opacity: 0;
+}
+.search-tansition.s-enter-to {
+  transform: translate3d(0, 0, 0);
+  opacity: 1;
+}
+.search-tansition.s-leave {
+  transform: translate3d(0, 0, 0);
+  opacity: 1;
+}
+.search-tansition.s-leave-to {
+  transform: translate3d(-10%, 0, 0);
+  opacity: 0;
 }
 </style>
