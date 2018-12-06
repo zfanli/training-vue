a604bad1da208ff9ddd47d7eeebcdbdaeda778ee
Richard <cyanyao@live.com>
2018-12-06 18:46:29 +0800

    add comments

--- src/utils/ga.js
+++ src/utils/ga.js
@@ -2,4 +2,6 @@ import Vue from 'vue'
 import ga from 'vue-analytics'
 import router from '../router'
 
+// Google Analytics support
+// Pass router instance for auto track
 Vue.use(ga, { id: 'UA-104770482-1', router })
